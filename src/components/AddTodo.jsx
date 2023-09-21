f1d9ee2c1d226c7441bb4649c48d5c72ba94880c
Arpana <arp2ctaula@gmail.com>
2023-09-21 14:22:46 +0545

    delete todo

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -12,13 +12,22 @@ const AddTodo = () => {
     setInput("");
   };
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mx-12">
+    <form
+      onSubmit={addTodoHandler}
+      className="space-x-3 mx-6 flex justify-center"
+    >
       <input
+        className="bg-slate-400 py-2 px-2 text-slate-50"
         placeholder="Enter a todo..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button type="submit">Add Todo</button>
+      <button
+        className="bg-cyan-600 px-2 hover:bg-cyan-800 text-cyan-50"
+        type="submit"
+      >
+        Add
+      </button>
     </form>
   );
 };
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 
 const Todos = () => {
   const todos = useSelector((state) => state.todos);
-  console.log({ todos });
+  const dispatch = useDispatch();
 
   return (
-  <div>
-    {todos.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
-    ))}
-  </div>
-  )
+    <div>
+      {todos.map((todo) => (
+        <div className="my-4" key={todo.id}>
+          {todo.text}{" "}
+          <button
+            className="mx-4 bg-red-600 px-2 py-1 text-white"
+            onClick={() => {
+              console.log(todo.id, "id of this todo");
+              dispatch(removeTodo(todo.id));
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Todos;
